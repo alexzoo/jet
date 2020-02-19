cff899f8e94c00430ff2296b4085cbdc941b9530
aleksey.zubov <aleksey.zubov@rt.ru>
2020-02-19 19:47:49 +0300

    initial

---
+++ api.py
@@ -0,0 +1,40 @@
+import requests
+import pytest
+from hamcrest import *
+
+
+def get_request(url, **kwargs):
+    return requests.get(url=url, params=kwargs)
+
+
+class TestHH:
+    hh_url = 'https://api.hh.ru/vacancies'
+
+    # test data for parametrize
+    text = ['test', 'qa']
+    experience = ['noExperience', 'between1And3']
+    vacancy_label = ['with_address', 'not_from_agency']
+    vacancy_type = ['open', 'anonymous']
+    vacancy_relation = ['favorited', 'got_response']
+
+    @pytest.mark.parametrize('text', text)
+    @pytest.mark.parametrize('experience', experience)
+    @pytest.mark.parametrize('vacancy_label', vacancy_label)
+    @pytest.mark.parametrize('vacancy_type', vacancy_type)
+    @pytest.mark.parametrize('vacancy_relation', vacancy_relation)
+    def test_hh_ru(self, text, experience, vacancy_label, vacancy_type, vacancy_relation):
+        # When
+        r = get_request(url=self.hh_url, text=text, experience=experience, vacancy_label=vacancy_label,
+                        vacancy_type=vacancy_type, vacancy_relation=vacancy_relation)
+        # That
+        assert_that(r.json(), has_items(anything(text)))
+        assert_that(r.json(), has_items(anything(experience)))
+        assert_that(r.json(), has_items(anything(vacancy_label)))
+        assert_that(r.json(), has_items(anything(vacancy_type)))
+        assert_that(r.json(), has_items(anything(vacancy_relation)))
+
+
+class TestBin:
+    bin_url = 'https://httpbin.org/put'
+
+    # test data
---
+++ ui.py
@@ -0,0 +1,27 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import ElementNotVisibleException
+
+
+class TestYandex:
+    def test_yandex(self):
+        driver = webdriver.Chrome('./chromedriver')
+        driver.wait = WebDriverWait(driver, 5)
+
+        driver.get("http://yandex.ru")
+        search_field = driver.find_element_by_xpath("//input[@id='text']")
+        search_field.send_keys("погода")
+        search_field.submit()
+
+        # try:
+        #     search_field = driver.wait.until(EC.element_to_be_clickable(By.XPATH, "//input[@id='text']"))
+        #     search_field.send_keys('погода')
+        # except TimeoutException:
+        #     print("Box or Button not found in google.com")
+        driver.quit()
